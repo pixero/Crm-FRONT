0d8a29ac46de5e65b29c93e4f60e4ec9eeffb7bc
Daniil Osipov <57679357+pixero@users.noreply.github.com>
2020-08-17 19:37:44 +0300

    add new dependins

---
+++ src/layouts/BasePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default class BasePage extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <p>Base page</p>
+            </div>
+        )
+    }
+}
